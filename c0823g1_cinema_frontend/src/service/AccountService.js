c411671f366cb8bc9942f3b7a72b5e1dd59d2eed
duydd-c0623g1 <daoducduy.work@gmail.com>
2024-03-05 10:05:00 +0700

    commit

--- c0823g1_cinema_frontend/src/service/AccountService.js
+++ c0823g1_cinema_frontend/src/service/AccountService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 export const getTopAccount = async () => {
-    let temp = await axios.get('')
+    let temp = await axios.get('http://localhost:8080/account/statistics?page=0')
     return temp.data
 }
 export const register = async (account) => {
